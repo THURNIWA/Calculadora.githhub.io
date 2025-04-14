bf45242dd2327ba28c4a9c76aebcdc32507af5c3
Niwa <arthurniwa94@gmail.com>
2025-04-14 19:03:39 -0300

    Soma dos numeros

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe o primeiro numero: ");
+            float num1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe o segundo numero: ");
+            float num2 = float.Parse(Console.ReadLine());
+
+            float resultado = num1 + num2;
+        }
+    }
+}
