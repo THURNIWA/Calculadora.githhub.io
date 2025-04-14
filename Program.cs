9769e0bdf9ca1bbdbd6d1030bff424f2c272783b
Niwa <arthurniwa94@gmail.com>
2025-04-14 19:13:06 -0300

    Subtração dos numeros

--- Program.cs
+++ Program.cs
@@ -6,6 +6,12 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
+            
+        }
+        static void Soma()
+        {
+            Console.Clear();
+            
             Console.WriteLine("Informe o primeiro numero: ");
             float num1 = float.Parse(Console.ReadLine());
 
@@ -13,6 +19,27 @@ namespace Calculator
             float num2 = float.Parse(Console.ReadLine());
 
             float resultado = num1 + num2;
+
+            Console.WriteLine("O resultado da soma é: " + resultado);
+
+            Console.ReadKey();
+        }
+
+        static void subtracao()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Informe o primeiro valor: ");
+            float num1 = float.Parse((Console.ReadLine()));
+
+            Console.WriteLine("Informe o segundo valor: ");
+            float num2 = float.Parse((Console.ReadLine()));
+
+            float resultado = num1 - num2;
+
+            Console.WriteLine("O resultado da subtração é: " + resultado);
+
+            Console.ReadKey();
         }
     }
 }
