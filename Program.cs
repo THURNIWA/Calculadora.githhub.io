b449fbd80b4cf78fbf92566ac964bf920af0ca5d
Niwa <arthurniwa94@gmail.com>
2025-04-14 19:21:18 -0300

    Divisão dos numeros

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace Calculator
         static void Soma()
         {
             Console.Clear();
-            
+
             Console.WriteLine("Informe o primeiro numero: ");
             float num1 = float.Parse(Console.ReadLine());
 
@@ -38,6 +38,22 @@ namespace Calculator
             float resultado = num1 - num2;
 
             Console.WriteLine("O resultado da subtração é: " + resultado);
+
+            Console.ReadKey();
+        }
+        static void divisao()
+        {
+            Console.Clear();
+            
+            Console.WriteLine("Informe o primeiro valor: ");
+            float num1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe o segundo numero: ");
+            float num2 = float.Parse(Console.ReadLine());
+
+            float resultado = num1 / num2;
+
+            Console.WriteLine("O resultado da divisão é: " + resultado);
 
             Console.ReadKey();
         }
